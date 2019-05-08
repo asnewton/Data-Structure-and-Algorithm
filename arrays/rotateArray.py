673fd19047f44b6c00e3d1523ae5b21956c682d2
ahmad ashraf <ahmadashrafdgp@gmail.com>
2019-05-08 15:12:42 +0530

    array rotation

---
+++ arrays/rotateArray.py
@@ -0,0 +1,15 @@
+# Python program to rotate an array by d elements 
+
+def rotateArray(arr, d, n):
+    for i in range(d):
+        temp = arr[0]
+        for j in range(1,n):
+            arr[j-1] = arr[j]
+        arr[j] = temp
+        
+    
+if __name__=="__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7]
+    n = len(arr)    
+    rotateArray(arr, 2, n)
+    print(arr)
